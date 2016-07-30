bdfbce06f6251df9cc0764e9e42c4831ca1a599d
Alexey Popravka <alexey.popravka@horsedevel.com>
2016-07-30 13:58:52 +0300

    more styling

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -9,15 +9,24 @@
     var ctx = document.querySelector('div.container');
     var play = ctx.querySelector('a.play');
     var stop = ctx.querySelector('a.pause');
+    var loader = ctx.querySelector('div.progress');
 
     var client = new Channel(CHANNEL_POPUP);
 
     client.addListener('stateChange', changeState);
     client.request('getState').then(changeState).catch((e) => console.log("error:", e))
 
-    play.addEventListener('click', (e) => client.notify('play', STREAM));
+    play.addEventListener('click', (e) => {
+      loader.classList.add('play');
+      client.notify('play', STREAM)
+    });
 
     stop.addEventListener('click', (e) => client.notify('stop'));
+
+    loader.addEventListener('webkitTransitionEnd', (e) => {
+      if(e.target.style.width == "100%")
+        e.target.classList.remove('play');
+    })
 
     // var settings = ctx.querySelector('a.settings');
     // settings.addEventListener('click', (e) => alert("Settings"));
@@ -42,12 +51,17 @@
       if (newState < STATE_PLAY_STARTING) {
         setState(false);
         setStateText('Stopped');
+        loader.style.width = "25%";
+      } else if (newState < STATE_WAITING) {
+        loader.style.width = "50%";
       } else if (newState < STATE_PLAYING) {
         setState(true);
         setStateText('Loading...');
+        loader.style.width = "75%";
       } else {
         setState(true);
         setStateText('Playing...');
+        loader.style.width = "100%";
       }
     }
 
